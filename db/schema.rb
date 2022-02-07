c9567d22c84262f6c76124cdd9d4015da228f850
crespire <li.simmon@gmail.com>
2022-02-07 14:59:05 -0500

    Complete blog tutorial

--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_02_07_191132) do
+ActiveRecord::Schema.define(version: 2022_02_07_195116) do
 
-  create_table "articles", force: :cascade do |t|
-    t.string "title"
-    t.text "body"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
-  end
+# Could not dump table "articles" because of following StandardError
+#   Unknown type 'sting' for column 'status'
 
   create_table "comments", force: :cascade do |t|
     t.string "commenter"
@@ -25,6 +21,7 @@ ActiveRecord::Schema.define(version: 2022_02_07_191132) do
     t.integer "article_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "status"
     t.index ["article_id"], name: "index_comments_on_article_id"
   end
 
